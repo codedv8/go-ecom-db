bf81bd5d8df8dd9389efbccef62e338b7f3ae72d
Thomas Björk <micoxdev@gmail.com>
2020-09-03 17:25:03 +0200

    Added some comments

--- db.go
+++ db.go
@@ -2,15 +2,23 @@ package EComDB
 
 import (
 	"context"
+	"log"
+
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"log"
 )
 
+// DBConnector - The struct that defines the database connector
 type DBConnector struct {
 	Client *mongo.Client
 }
 
+// NewDBConnector - Create a new instance of a database conenctor
+func NewDBConnector() *DBConnector {
+	return &DBConnector{}
+}
+
+// Init - Initialize the database connector
 func (dbc *DBConnector) Init() bool {
 	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017/")
 	client, err := mongo.Connect(context.TODO(), clientOptions)
